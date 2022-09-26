1b4537a284542fcb0eafef963d5566de9dcd0f3b
Largehawiian <jeremyorama@gmail.com>
2022-09-26 11:32:01 -0400

    Commit

---
+++ Search-Google.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    Search Google from the command line.
+.DESCRIPTION
+    Search Google from the command line.
+.NOTES
+    
+.LINK
+    
+.EXAMPLE
+    Search-Google.ps1 -Query "PowerShell" -ApkiKey "Your  Key"
+#>
+Function Search-Google{
+    param (
+        [Parameter(Mandatory=$true, Position=0)][string]$Query,
+        [Parameter(Mandatory=$true, Position=1)][string]$APIKey
+    )
+
+    $cx ="c3062c84ab15f4527"
+    $results = invoke-restmethod -uri ("https://customsearch.googleapis.com/customsearch/v1?key={1}&q={0},&cx={2}" -f $Query,$Key,$cx) -Method Get -ContentType "application/json"
+    $results.items | ForEach-Object {
+        [PSCustomObject]@{
+            Title = $_.title
+            Link = $_.link
+            Snippet = $_.snippet
+        }
+    }
+}
